bc9de9181376277cfe2304bb64a0329738f0fef5
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-05-17 20:17:00 -0400

    Add project files.

---
+++ CraveWheels/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CraveWheels.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //GET: /shop/Restaurant
+        public IActionResult Restaurant() 
+        { 
+            return View(); 
+        }
+    }
+}
