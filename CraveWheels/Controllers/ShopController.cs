7ed17e412f58d726b635a4aafb8f07e896e57780
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-05-21 02:14:37 -0400

    added comment to check new loaction of project is working

--- CraveWheels/Controllers/ShopController.cs
+++ CraveWheels/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CraveWheels.Models;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace CraveWheels.Controllers
 {
@@ -9,9 +11,18 @@ namespace CraveWheels.Controllers
             return View();
         }
         //GET: /shop/Restaurant
-        public IActionResult Restaurant() 
-        { 
-            return View(); 
+        public IActionResult Restaurant(String Name)
+        {
+            if (Name == null) { 
+                return RedirectToAction("Index");
+            }
+            // use the product model to make a mock list of product to display
+            var products = new List<Product>();
+            for (var i=1;i<11;i++) { 
+                products.Add(new Product { ProductID=i,Name = "Product" + i.ToString() ,Price=i});
+            }
+            ViewData["Name"] = Name;
+            return View(products); 
         }
     }
 }
