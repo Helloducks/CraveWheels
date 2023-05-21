356baacdff92945e33bc469161e5e6e24bdbb43d
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-05-21 02:17:01 -0400

    var add to check vaildity of new loaction

--- CraveWheels/Models/Product.cs
+++ CraveWheels/Models/Product.cs
@@ -1,9 +1,10 @@
 ﻿namespace CraveWheels.Models
 {
     public class Product
-    {
+    { //this comment here to check updates
         public int ProductID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public decimal testvar { get; set; }
     }
 }
